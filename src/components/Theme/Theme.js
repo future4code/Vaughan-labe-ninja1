7c4517eb2cc5300360dce04a4535e212547ecfa7
Vsux <vsux17@gmail.com>
2022-02-01 15:35:59 -0200

    theme

---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "./@chakra-ui/react";
+export const theme = extendTheme({
+    colors: {
+        brand:{
+            900: '#FC7C01',
+            800: '#F6AF56',
+            700: '#A8BAC5',
+            600: '#45525B'
+        }
+    }
+})
