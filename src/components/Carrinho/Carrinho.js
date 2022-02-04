0ac2c6fa668dacf292f7aaf49d60c174c75dce63
Vsux <vsux17@gmail.com>
2022-02-04 16:51:32 -0200

    carrinho

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -8,36 +8,36 @@ const Container = styled.div`
   width: 300px;
 `
 
-export default class Carrinho extends React.Component{
+export default class Carrinho extends React.Component {
 
     state = {
-        cards:[
-            {   
+        cards: [
+            {
                 id: 1,
                 titulo: "Aulas de Francês",
-                preco:125,
-                data:"04/01/2022",
+                preco: 125,
+                data: "04/01/2022",
                 quantidade: 1
             },
             {
                 id: 2,
                 titulo: "Aulas de Sei Lá",
-                preco:355,
-                data:"06/02/2022",
+                preco: 355,
+                data: "06/02/2022",
                 quantidade: 1
             },
             {
                 id: 3,
                 titulo: "Aulas de alguma coisa",
-                preco:500,
-                data:"14/03/2022",
+                preco: 500,
+                data: "14/03/2022",
                 quantidade: 1
             },
             {
                 id: 4,
                 titulo: "Aulas de dfdgfdgdfgh",
-                preco:500,
-                data:"14/03/2022",
+                preco: 500,
+                data: "14/03/2022",
                 quantidade: 1
             }
         ]
@@ -53,46 +53,46 @@ export default class Carrinho extends React.Component{
 
     removerProduto = (id) => {
         let produto = this.state.cards.filter(produto => {
-            return  produto.id === id
-         })[0]
+            return produto.id === id
+        })[0]
         const indiceCard = this.state.cards.indexOf(produto)
         const arrayNovoCard = [...this.state.cards]
-           arrayNovoCard.splice(indiceCard, 1)
-        this.setState({cards: arrayNovoCard})
+        arrayNovoCard.splice(indiceCard, 1)
+        this.setState({ cards: arrayNovoCard })
     }
 
     alertaDeCompra = () => {
         alert("Compra Finalizada!")
     }
-    render(){
-        return(
-            
-            
+    render() {
+        return (
+
+
             <div>
-                
+
                 <h1>Carrinho:</h1>
-                
-              <Container>
-                  
-                {this.state.cards.map((produto) => {
-                    
-                    return (
-                        <div key={produto.title}>
-                            <p>Produto: {produto.titulo}</p>
-                            <button onClick={() => this.removerProduto(produto.id)} type="submit">
-                                Remover
-                            </button>
-                        </div>
-                    )
-                })}
+
+                <Container>
+
+                    {this.state.cards.map((produto) => {
+                        
+                        return (
+                            <div  key={produto.id} title={produto.title} price={produto.price} id={produto.id} removerProduto={removerProduto}>
+                                <p>Produto: {produto.titulo}</p>
+                                <button onClick={() => this.removerProduto(produto.id)} type="submit">
+                                    Remover
+                                </button>
+                            </div>
+                        )
+                    })}
                 </Container>
                 <h2>Valor Total: R$ {this.pegarValorTotal()} ,00</h2>
                 <button onClick={this.alertaDeCompra}>Finalizar Compra</button>
                 <button onClick={this.props.telaCard}>Voltar para a lista de serviços </button>
-                
+
             </div>
         )
-    }        
-        
-    
+    }
+
+
 }
